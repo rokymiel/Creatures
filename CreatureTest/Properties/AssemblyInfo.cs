ff151c783b6a3e889983d74b2bcdcfebb33eb7ca
rokymiel <rokymiel@gmail.com>
2020-04-20 01:08:32 +0300

    Начальный возврат модуля HomeWorkTest

---
+++ CreatureTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CreatureTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CreatureTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("141207d6-b951-48bc-9493-8138645b94bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CreatureTest/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using CreaturesLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CreatureTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestToString()
+        {
+            Creature a = new Creature("Mambad", MovementType.Walking, 3.7689);
+            Assert.AreEqual(a.ToString(), "Walking creature Mambad: Health = 3,769");
+        }
+        [TestMethod]
+        public void TestOperator()
+        {
+            Creature am = new Creature("Amadeus", MovementType.Swimming, 7.3);
+            Creature be = new Creature("Bethov", MovementType.Swimming, 8.7);
+            Assert.AreEqual(am * be, new Creature("Amadhov", MovementType.Swimming, (7.3 + 8.7) / 2));
+        }
+        [TestMethod]
+        public void TestObjectSerialaize()
+        {
+            string path = "test.xml";
+            Creature am = new Creature("Amadeus", MovementType.Swimming, 7.3);
+            using (FileStream writer = new FileStream(path, FileMode.Create))
+            {
+                DataContractSerializer ser = new DataContractSerializer(am.GetType());
+                ser.WriteObject(writer, am);
+            }
+            Creature newAm;
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                DataContractSerializer ser = new DataContractSerializer(typeof(Creature));
+                newAm = (Creature)ser.ReadObject(fs);
+            }
+            Assert.AreEqual(am,newAm);
+        }
+
+    }
+}
